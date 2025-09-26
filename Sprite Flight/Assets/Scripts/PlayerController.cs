31939783ac99d79b39c837183e8c960c9060779e
kjbrown24 <kjbrown@willamette.edu>
2025-09-25 23:55:17 -0700

    Save 6
    SCORE BOARD

--- Sprite Flight/Assets/Scripts/PlayerController.cs
+++ Sprite Flight/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    private float elapsedTime = 0f;
+    private float score = 0f;
+    public float scoreMultiplier = 10f;
     public GameObject boosterFlame;
    public float maxSpeed = 5f;
    public float thrustForce = 1f;
     Rigidbody2D rb;
+    public UIDocument uiDocument;
    
+   private Label scoreText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
     }
 
     // Update is called once per frame
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+        score = elapsedTime * scoreMultiplier;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
+        Debug.Log("Score: " + score);
+        scoreText.text = "Score: " + score;
+        
         if (Mouse.current.leftButton.isPressed) {
             // Calculate mouse direction
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
